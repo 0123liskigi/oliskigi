3d1cf4310d0fe27b7e23e5b31cdd1e038fa68684
0123liskigi <77651380+0123liskigi@users.noreply.github.com>
2022-08-19 16:47:57 +0800

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,3 +1,7 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/0123liskigi/oliskigi/work/93132819.lua", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vodxn/sape/main/Initiate.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/2b3cc954d773907cb24c4e889a365097e23e33f9/CustomModules/6872274481.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0123liskigi/oliskigi/work/420420420.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0123liskigi/oliskigi/work/9318410841.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0123liskigi/oliskigi/work/718571748.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0123liskigi/oliskigi/work/6905810851.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0123liskigi/oliskigi/work/6969696969.lua", true))()
