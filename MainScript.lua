d6edf57b9eab66d9c8cf0bdae341cd12d3a34ec0
0123liskigi <77651380+0123liskigi@users.noreply.github.com>
2022-08-19 16:30:37 +0800

    Create MainScript.lua

---
+++ MainScript.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0123liskigi/oliskigi/work/93132819.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vodxn/sape/main/Initiate.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/2b3cc954d773907cb24c4e889a365097e23e33f9/CustomModules/6872274481.lua", true))()
